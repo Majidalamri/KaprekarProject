3d9b737c8168c1822c1cb5287d0245ee867dd0f8
Majidalamri <54560844+Majidalamri@users.noreply.github.com>
2019-09-11 19:19:41 -0500

    changing the assignment to true statement (==)

--- c_version/kaprekarUtils.c
+++ c_version/kaprekarUtils.c
@@ -33,7 +33,7 @@ int isKaprekar(int n) {
   //compute the square
   long square = n * (long) n;
   //compute the number of digits of the square
-  int numDigits = (int) log10(n) + 1;
+  int numDigits = (int) log(n) + 1;
   long modulus = 0;
   long first, second;
 
@@ -51,7 +51,7 @@ int isKaprekar(int n) {
     //test if the split makes a Kaprekar number
     //the second part cannot be zero and if
     //the sum of the two parts is equal to n, then it is
-    if(second > 0 && n = first + second) {
+    if(second > 0 && n == first + second) {
       return 1;
     }
   }
